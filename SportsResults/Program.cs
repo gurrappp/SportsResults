cef3e98022daa93dc1cb4c69d34ed33f84fa95f7
Gustav Polhem <gustavpolhem@gmail.com>
2024-07-13 22:33:16 +0200

    Add project files.

---
+++ SportsResults/Program.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+
+
+
+namespace SportsResults
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.basketball-reference.com/boxscores/");
+
+            var test = document.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
+
+            Console.WriteLine(test);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
+
